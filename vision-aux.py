2e6c2864c3645b5b9666c554100d06d4fa020667
dan <outnumbered@live.com>
2019-03-22 00:45:48 -0400

    first photos

--- vision-aux.py
+++ vision-aux.py
@@ -4,12 +4,14 @@ from PIL import Image
 import numpy as np
 import time
 from pynput.mouse import Button, Controller as MouseController
+from pynput.keyboard import Key, Controller
 
 def convert_rgb_to_bgr(img):
         return img[:, :, ::-1]
 
 
 mouse = MouseController()
+keyboard = Controller()
 
 resolution = mss().monitors[0]  #get the screen resolution
 # print(resolution)
@@ -38,17 +40,20 @@ img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 #     cv2.rectangle(img_gray, pt, (pt[0] + w, pt[1] + h), (255,255,255), 4)
 
 
-chair_1 = cv2.imread('assets/chair1.png',0)
+chair_1 = cv2.imread('assets/floor1.png',0)
 w, h = chair_1.shape[::-1] # weight height
 
 res = cv2.matchTemplate(img_gray, chair_1, cv2.TM_CCOEFF_NORMED)
 
-threshold = 0.9
+threshold = 0.5
 loc = np.where( res >= threshold)
 loczip = list(zip(*loc[::-1]))
 coor = loczip[0]
 mouse.position = (coor[0]+int(w/2), coor[1]+int(h/2))
 mouse.click(Button.left)
+keyboard.type('Hello World')
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
 
 for pt in zip(*loc[::-1]):
     # print(pt)
@@ -56,6 +61,6 @@ for pt in zip(*loc[::-1]):
     cv2.rectangle(img_gray, pt, (pt[0] + w, pt[1] + h), (255,255,255), 4)
 
 # cv2.imshow('img_bgr', img_gray)
-# # cv2.imshow('habbo', habbo_avatar_1)
+# # # cv2.imshow('habbo', habbo_avatar_1)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
